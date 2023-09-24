bc4872c6e4a86492f7a8aafa471b4cf01ba92a7b
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2023-09-24 13:18:17 -0500

    arreglo jcalendar en el proyecto, colocar dir imagenes en resources

--- src/main/java/views/Login.java
+++ src/main/java/views/Login.java
@@ -6,8 +6,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import java.awt.BorderLayout;
-import javax.swing.JInternalFrame;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
