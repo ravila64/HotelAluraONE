5ff29a1a146344523ba5bb4177fa5fd748c8665c
Rene Avila <rene.avila.alonso@gmail.com>
2023-09-24 20:38:28 -0500

    arreglo tablas huespedes + reservas

--- src/main/resources/db/migration/V2__create-table-huespedes.sql
+++ src/main/resources/db/migration/V2__create-table-huespedes.sql
@@ -1,11 +1,11 @@
 create table huespedes(
     Id int not null auto_increment,
-	Nombre varchar(80) not null,
-	Apellido varchar(80) not null,
-	FechaNacimiento date not null,
-	Nacionalidad varchar(50) not null,
-	Telefono varchar(30) not null,
-	IdReserva int not null,
+	nombre varchar(80) not null,
+	apellido varchar(80) not null,
+	fecha_nacimiento date not null,
+	nacionalidad varchar(50) not null,
+	telefono varchar(30) not null,
+	id_reserva int not null,
 	primary key(Id),
-	foreign key(IdReserva) references reservas(id)
+	foreign key(id_reserva) references reservas(id)
 );
